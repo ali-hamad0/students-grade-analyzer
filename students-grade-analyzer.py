4fc3d06fde263783d7d210ed5a75f8a633a58786
ali-hamad0 <165719525+ali-hamad0@users.noreply.github.com>
2025-06-11 01:32:04 +0300

    Update students-grade-analyzer.py

--- students-grade-analyzer.py
+++ students-grade-analyzer.py
@@ -1,9 +1,10 @@
 #1- Prints students names and grades
 
 def display_student_summary(stnames, grades):
-    print("here are the students names and their grades : ")
+    print("Here are the students names and their grades : ")
+    print("      ")
     for x in range(len(stnames)):
-        print(stnames[x] + "->" + str(grades[x]))
+        print("      " + stnames[x] + " |-> " + str(grades[x]))
 
 #2- get the average grade of the class
 
@@ -34,7 +35,6 @@ def count_passed(grades):
             count += 1
     return count
 
-#main
 
 num_of_students =  int(input("Enter the number of students"))
     
@@ -44,20 +44,21 @@ grades = []
 for i in range(num_of_students):
         name = input("Enter student name : " )
         grade = int(input("Enter " + name + "'s grade : "))
+        while grade < 0 or grade > 100:
+            print(" Please enter a grade between 0 and 100.")
+            grade = int(input("Enter " + name + "'s grade : "))
         stnames.append(name)
         grades.append(grade)
 
 display_student_summary(stnames , grades)
 
 avg_grades = get_avg_grade(grades)
-print("the average grade of the class is : " + str(avg_grades))
+print("The average grade of the class is : " + str(avg_grades))
+print("")
 
 highest_name, highest_grade = get_highest_grade(stnames,grades)
-print("the highest grade is :" + str(highest_grade)  + " and it's earned by :" + highest_name )
+print("The highest grade is :" + str(highest_grade)  + " and it's earned by :" + highest_name )
+print("")
 
 count_pass = count_passed(grades)
-print("the number of students who passed (with grade >=60) is : " + str(count_pass))
-
-
-        
-
+print("The number of students who passed (with grade >=60) is : " + str(count_pass))
